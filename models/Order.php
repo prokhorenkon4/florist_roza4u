539e2ce3eaf730a217b6c32b9d05f424bc22095c
Nikolai Prokhorenko <prokhorenko.n.a@yandex.ru>
2023-09-05 22:01:50 +0500

    edit model Oreder.php add 2 functions

--- models/Order.php
+++ models/Order.php
@@ -9,9 +9,18 @@ use yii\db\ActiveRecord;
 
 class Order extends ActiveRecord
 {
-    public function getAll(){
+    public function getAll()
+    {
         return Order::find()->all();
     }
+    public function getOrderProducts()
+    {
+        return $this->hasMany(OrderProduct::class, ['order_id' => 'id']);
+    }
 
+    public function getOrderPhotos()
+    {
+        return $this->hasMany(OrderPhoto::class, ['order_id' => 'id']);
+    }
 
 }
